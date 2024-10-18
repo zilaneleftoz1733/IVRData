a2d9e7e08683f428fa6ce0f1a6f0781544e8af46
zilanelefoz <lzilaneleftozl@gmail.com>
2024-10-18 10:28:15 +0300

    Telefon Numaralarına gelen aramalar listelendi

---
+++ IvrConsole/IvrLog.cs
@@ -0,0 +1,21 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IvrConsole
+{
+    [Table("IvrLog")]
+    public class IvrLog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ArayanNo { get; set; }
+        public string ArananNo { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int IvrNo { get; set; }
+    }
+}
--- IvrConsole/Program.cs
+++ IvrConsole/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace IvrConsole
+﻿using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+
+namespace IvrConsole
 {
     internal class Program
     {
+       public static Random rdn = new Random();
         static void Main(string[] args)
         {
+            string constr = @"server=DESKTOP-2KPKJ12;Database=TicariPaket;Trusted_Connection=true;TrustServerCertificate=true";
+
+            List<string> phoneNumber = new List<string>()
+            {
+                "0850 750 00 00",
+                 "0850 750 10 00",
+                  "0850 750 20 00",
+                   "0850 750 30 00",
+                    "0850 750 40 00",
+                     "0850 750 50 00",
+            };
+            
+            SqlConnection db = new SqlConnection(constr);
+            for (int i = 0; i < 1000; i++)
+            {
+                var arama = new IvrLog
+                {
+                    ArayanNo = GenerateNumber(),
+                    ArananNo = phoneNumber[rdn.Next(0,6)],
+                    StartDate=DateTime.Now.AddDays(-2),
+                    EndDate=DateTime.Now.AddDays(-2).AddMinutes(rdn.Next(1, 10)),
+                    IvrNo=rdn.Next()
+                }; 
+                db.Insert<IvrLog>(arama);
+            }
+            //db.InsertAsync<IvrLog>();
             Console.WriteLine("Hello, World!");
         }
+        public static string GenerateNumber()
+        {
+            return "5" + rdn.Next(0, 9).ToString() + rdn.Next(0, 9).ToString()
+                +rdn.Next(0, 9).ToString() + rdn.Next(0, 9).ToString()
+                + rdn.Next(0, 9).ToString() + rdn.Next(0, 9).ToString();
+        }
+
     }
 }
